05a8e9bb3944e1336fb4f7d4eb9f602fb8187918
Samueloslo <95286051+Samueloslo@users.noreply.github.com>
2021-11-30 13:36:20 +0100

    Opdater main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -5,28 +5,30 @@ input.onButtonPressed(Button.B, function () {
     Yværdi += 1
 })
 let Xværdi = 0
-let Yværdi = 3
-Yværdi = 3
+let Yværdi = 0
+led.plot(Xværdi, Yværdi)
+loops.everyInterval(500, function () {
+    if (true) {
+    	
+    }
+})
 basic.forever(function () {
     if (input.buttonIsPressed(Button.A) || input.buttonIsPressed(Button.B)) {
         basic.clearScreen()
+        basic.pause(200)
         led.plot(Xværdi, Yværdi)
     }
 })
 basic.forever(function () {
-    if (Xværdi == 6) {
-        Xværdi = 1
+	
+})
+basic.forever(function () {
+    if (Xværdi == 5) {
+        Xværdi = 0
     }
 })
 basic.forever(function () {
-    if (Yværdi == 6) {
-        Yværdi = 1
-    }
-})
-basic.forever(function () {
-    if (true) {
-    	
-    } else {
-    	
+    if (Yværdi == 5) {
+        Yværdi = 0
     }
 })
